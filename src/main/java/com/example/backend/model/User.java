9ac0bf3d698e86c3ead9bc9166564fef0400d8d7
Rusiru Erandaka <erandak-ec20037@stu.kln.ac.lk>
2024-11-11 22:46:16 +0530

    User and mail service update

---
+++ src/main/java/com/example/backend/model/User.java
@@ -0,0 +1,26 @@
+package com.example.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class User extends Muser{
+
+    @Transient
+    public static final String SEQUENCE_NAME="user_sequence";
+
+    @Id
+    private String user_id;
+
+
+
+}
