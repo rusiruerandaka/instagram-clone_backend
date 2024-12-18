51d207f20e9b297042b63e76f817b0a862d7180f
Tharanga Madurapperuma <tharanga.s.madurapperuma@gmail.com>
2024-12-19 04:14:16 +0530

    Delete FileStore repository

--- src/main/java/com/example/backend/repository/FileStoreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.backend.model.FileStore;
-
-public interface FileStoreRepository extends MongoRepository<FileStore, String> {
-    FileStore findByName(String name);
-}
