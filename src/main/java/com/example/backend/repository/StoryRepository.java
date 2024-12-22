7af1151215e51b36ab65b2b52c9d11d3608f4f21
tharanga-madurapperuma <tharanga.s.madurapperuma@gmail.com>
2024-12-22 21:58:22 +0530

    Add findById method to repository

--- src/main/java/com/example/backend/repository/StoryRepository.java
+++ src/main/java/com/example/backend/repository/StoryRepository.java
@@ -1,7 +1,12 @@
 package com.example.backend.repository;
 
 import com.example.backend.model.Story;
+
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface StoryRepository extends MongoRepository<Story, String> {
+    @SuppressWarnings("null")
+    Optional<Story> findById(String id);
 }
