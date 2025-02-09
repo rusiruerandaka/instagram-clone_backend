f6d32176eaf120868cd7f5f1d39584b00fcd3a44
tharanga-madurapperuma <tharanga.s.madurapperuma@gmail.com>
2025-02-09 09:21:04 +0530

    add swagger ui

--- src/main/java/com/example/backend/config/SecurityConfig.java
+++ src/main/java/com/example/backend/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:8080/swagger-ui/index.html#"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
@@ -99,11 +99,12 @@ public class SecurityConfig {
     //     return http.build();
     //}
 
+    @SuppressWarnings("deprecation")
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeRequests(authorize -> authorize
-                .requestMatchers("/users/login", "/users/register", "/users/oauth2/login", "/users/oauth2/login/fb").permitAll()
+                .requestMatchers("/users/login", "/users/register", "/users/oauth2/login", "/users/oauth2/login/fb","/swagger-ui/**","/v3/api-docs/**", "/swagger-ui.html").permitAll()
                 .anyRequest().authenticated()
             )
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
